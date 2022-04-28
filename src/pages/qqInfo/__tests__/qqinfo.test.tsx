afea8bf70d41643e02dd981a75470dca89a1feed
ext.lijinglong <ext.lijinglong@jd.com>
2022-04-28 19:05:13 +0800

    feat: 单测开发

--- src/pages/qqInfo/__tests__/qqinfo.test.tsx
+++ src/pages/qqInfo/__tests__/qqinfo.test.tsx
@@ -1,7 +1,35 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import QQInfo from "../index";
-describe("", () => {
-  test("", () => {
+describe("QQInfo => 查询qq用户头像信息页面测试", () => {
+  test("模拟用户输入qq长度为小于5非法qq校验 查询不执行", () => {
+    jest.useFakeTimers();
+    const { container } = render(<QQInfo />);
+    const input = container.querySelector<HTMLInputElement>("input");
+    if (!input) return;
+    userEvent.type(input, "1234");
+    act(() => {
+      jest.runAllTimers();
+    });
+    // qq好长度为4不请求数据直接 loading状态值没有变化 依旧为空
+    const loading = container.querySelector<HTMLInputElement>(".loading");
+    // 断言loading组件为null
+    expect(loading).toBeNull();
+  });
+  test("模拟用户输入qq长度为12 输入只能为12为", () => {
+    jest.useFakeTimers();
+    const { container } = render(<QQInfo />);
+    const input = container.querySelector<HTMLInputElement>("input");
+    if (!input) return;
+    userEvent.type(input, "012345678912345");
+    act(() => {
+      jest.runAllTimers();
+    });
+    // 断言输入长度只能为12
+    expect(input.value).toBe("012345678912");
+  });
+  test("用户输入合法qq号 校验显示结果是否正常", async () => {
     jest.doMock("../../../api", () => {
       return {
         getQQInfo: Promise.resolve({
@@ -12,9 +40,18 @@ describe("", () => {
         }),
       };
     });
+    jest.useFakeTimers();
     const { container } = render(<QQInfo />);
-    expect(
-      container.querySelector(".loading") as HTMLDivElement
-    ).toBeEmptyDOMElement();
+    const input = container.querySelector<HTMLInputElement>("input");
+    if (!input) return;
+    userEvent.type(input, "1234567");
+    act(() => {
+      jest.runAllTimers();
+    });
+    await waitFor(() => {
+      const avatar = container.querySelector<HTMLDivElement>(".loading");
+      if (!avatar) return;
+      // expect(avatar).toBeEmptyDOMElement();
+    });
   });
 });
